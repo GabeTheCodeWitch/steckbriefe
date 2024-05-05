45665ff30712f356b775e1e0fd6a137ad7f0fb98
Gabe <k_muenchow@gmx.de>
2024-05-05 19:01:55 +0200

    mutanten infos

---
+++ src/routes/gabe/mutanten/+page.svelte
@@ -0,0 +1,13 @@
+<h1>Mutanten haben...</h1>
+<ul>
+  <li>zusätzlichen Chromosomenpaar - das τ-Chromosom (tau)</li>
+  <li>tau-von Transmutation, der chemischen Umwandlung von Qualität und Sorte</li>
+  <li>rezesives Gen, dihybrider Erbgang</li>
+  <li>das Mutantengen wird also nur selten vererbt</li>
+</ul>
+
+<h1>Die Welt...</h1>
+<ul>
+  <li>ist in zwei Nationen geteilt</li>
+  <li>Mutanten schleusen andere Mutanten von nord nach sü´in Sicherheit</li>
+</ul>
