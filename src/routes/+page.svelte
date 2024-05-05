b55f7395283a999b79b018222aede3d3b3a586db
Gabe <k_muenchow@gmx.de>
2024-05-05 10:30:15 +0200

    ubersicht page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<div class="user-cards">
+  <a class="user-card" href="/sauriel"> <h1>Sauriel</h1> </a>
+  <a class="user-card" href="/gabe"> <h1>Gabe</h1> </a>
+</div>
+
+<style>
+  .user-cards {
+    margin: 5rem;
+    display: flex;
+    gap: 2em;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .user-card {
+    width: 20vw;
+    aspect-ratio: 3/2;
+    background-color: var(--blur);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: var(--animation);
+  }
+
+  .user-card:hover {
+    background-color: var(--transparent);
+  }
+</style>
